48308b533b2754ea70192f4ef15690029602a367
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-02-03 00:43:35 +0500

    send and receive data from API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import './style.css';
-import Function from './modules/function.js';
+import Object from './modules/object.js';
+import * as API from './modules/Api.js';
+import element from './modules/element.js';
 
-const functions = new Function();
+document.querySelector('.refresh-button').addEventListener('click', (async () => {
+  const data = await API.get();
+  element(data);
+}));
+
+const userInput = document.querySelector('input[type="text"]');
+const scoreInput = document.querySelector('input[type="number"]');
 
 document.querySelector('.form-submit-button').addEventListener('click', (() => {
-  functions.add();
+  if (userInput.value !== '' && scoreInput.value !== '') {
+    const object = new Object(userInput.value, scoreInput.value);
+    API.post(object);
+    userInput.value = '';
+    scoreInput.value = '';
+  }
 }));
---
+++ src/modules/object.js
@@ -0,0 +1,8 @@
+class Object {
+  constructor(user, score) {
+    this.user = user;
+    this.score = score;
+  }
+}
+
+export default Object;
